e2b344e1b34b0e11eabaa0fdd035b54ffdf7e09f
ZarkxWorld <43546157+ZarkxWorld@users.noreply.github.com>
2019-02-19 01:35:56 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Bootloader from './bootloader.js';
+import Play from './scenes/play.js';
+import Gameover from './scenes/gameover.js';
+import UI from './scenes/UI.js';
+import Menu from './scenes/menu.js';
+
+const config = {
+    title: 'Snake',
+    width: 320,
+    height: 180,
+    type: Phaser.AUTO,
+    parent: 'container',
+    backgroundColor: '#000000',
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        // arcade: {
+        //     gravity: { y: 100 }
+        // }
+    },
+    scene: [Bootloader, Play, Gameover, UI, Menu]
+};
+
+new Phaser.Game(config);
